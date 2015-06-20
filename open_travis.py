9be680e8f27ee1035459c2e87534cc3f455fc4b9
Cethric <Cethric@users.noreply.github.com>
2015-06-20 01:57:45 +0000

    Update README.md and add helper script for Pythonista .travis.yml file

---
+++ open_travis.py
@@ -0,0 +1,16 @@
+import editor
+import os
+
+CONTENTS = '''language: python
+python:
+     - "2.6"
+     - "2.7"
+
+script: nosetest
+'''
+path = os.path.abspath(".travis.yml")
+print path
+#with open(path, "wb") as f:
+#    f.write(CONTENTS)
+    
+editor.open_file("HTMLEditor-Pythonista/.travis.yml");
