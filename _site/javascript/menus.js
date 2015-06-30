2b5e15eeeb80de7f40210b211f31f0f150e69779
Cethric <roganBlake@gmail.com>
2015-06-30 15:39:06 +1000

    Automated Update

--- _site/javascript/menus.js
+++ _site/javascript/menus.js
@@ -28,6 +28,28 @@ document.onkeydown = function(event) {
     }
 }
 
+document.ontouch = function(event){
+    var list_parent = false;
+    var more_parent = false;
+    for(var node = event.target; node != document.body; node = node.parentNode)
+    {
+        if(node.id == 'projects_item'){
+            list_parent = true;
+            break;
+        } else if (node.id == 'more_menu') {
+            more_parent = true;
+        }
+    }
+    if(list_parent)
+        list_handle();
+    else
+        list.style.display = 'none';
+    if(more_parent)
+        more_menu();
+    else
+        posts.style.right = "-50em";
+}
+
 function list_handle() {
     if (list.style.display=='none') {
         list.style.display = 'block';
